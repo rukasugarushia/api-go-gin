a4956b7fd80c9720c2eda39943c5fb369d736daa
Lucas Martins <lucas.martins@reclameaqui.com.br>
2023-02-24 15:06:55 -0300

    first commit

---
+++ routes/routes.go
@@ -0,0 +1,18 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/rukasugarushia/api-go-gin/controllers"
+)
+
+func HandleRequests() {
+	r := gin.Default()
+	r.GET("/alunos", controllers.ExibeTodosAlunos)
+	r.GET("/:nome", controllers.Saudacao)
+	r.POST("/alunos", controllers.CriaNovoAluno)
+	r.GET("/alunos/:id", controllers.BuscaAlunoPorId)
+	r.DELETE("/alunos/:id", controllers.DeletaAluno)
+	r.PATCH("/alunos/:id", controllers.EditaAluno)
+	r.GET("/alunos/cpf/:cpf", controllers.BuscaAlunoPorCPF)
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
